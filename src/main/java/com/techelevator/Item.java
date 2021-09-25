ad2bbf7b30ead4a3888e7ca2a662eb5d326e8317
unknown <matthew.min96@gmail.com>
2021-09-25 15:25:25 -0400

    version 1

---
+++ src/main/java/com/techelevator/Item.java
@@ -0,0 +1,20 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Item {
+    String name;
+    String slot;
+    int quantity;
+    double price;
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return  price;
+    }
+}
--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,16 +2,37 @@ package com.techelevator;
 
 import com.techelevator.view.Menu;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
+	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+
+	private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
+
 	private Menu menu;
 
 	public VendingMachineCLI(Menu menu) {
 		this.menu = menu;
+	}
+
+	public void getQuantity(int quantity){
+		if(quantity > 0){
+			//Subtract inventory purchased from quantity
+			//
+		}
+		else{
+			System.out.println("SOLD OUT");
+		}
+
 	}
 
 	public void run() {
@@ -20,8 +41,24 @@ public class VendingMachineCLI {
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				// display vending machine items
+
+				File file = new File("vendingmachine.csv");
+				int quantity = 5;
+
+				try(Scanner vmi = new Scanner(file)){
+					while(vmi.hasNext()){
+						String[] values = vmi.nextLine().split("\\|");
+						System.out.println(values[0] + " | " + values[1] + " | " + values[2] + " | " + "Available: " + quantity );
+					}
+
+				}
+				catch (FileNotFoundException e){
+					System.out.println("Vending machine list not found.");
+				}
+
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
+
 			}
 		}
 	}
